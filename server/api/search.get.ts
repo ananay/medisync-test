23ec358cb869bcaeac88affed6ece31539f42039
ananay <i@ananayarora.com>
2026-07-05 15:01:55 -0700

    Add comment to search endpoint

--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -1,4 +1,5 @@
 import { getDb } from '../utils/db'
+// Global search across patients, records, and prescriptions
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
